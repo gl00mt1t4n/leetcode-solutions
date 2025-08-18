97513e6b15f022b729fd2be43195bf9be68e48bb
gl00mt1t4n <gloomtitan1337@gmail.com>
2025-08-18 16:14:30 +0530

    Time: 6 ms (45.52%), Space: 59.5 MB (7.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,7 +1,13 @@
 function twoSum(nums: number[], target: number): number[] {
-    for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) return [i, j];
-        }
+// hashmap implementation
+// we make a map of every number and its index, then lookup the complement (target minus number)
+
+    let map: Map<number, number> = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        map.set(nums[i], i);
+    }
+    for (let i = 0; i <nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement) && map.get(complement) !== i) return [i, map.get(complement)];
     }
 };

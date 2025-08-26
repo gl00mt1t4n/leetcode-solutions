cac68a69b5bb31d9749d2e5e254a305296e267d1
gl00mt1t4n <gloomtitan1337@gmail.com>
2025-08-26 21:47:27 +0530

    Time: 3 ms (28.57%), Space: 58.5 MB (28.57%) - LeetHub

---
+++ 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.ts
@@ -0,0 +1,17 @@
+function areaOfMaxDiagonal(dimensions: number[][]): number {
+    let maxDiagonal = 0;
+    let maxArea = 0;
+    for (let [w, h] of dimensions) {
+        let diagonal = Math.sqrt(w * w + h * h);
+        let area = w * h;
+
+        if (diagonal > maxDiagonal) {
+            maxDiagonal = diagonal;
+            maxArea = area;
+        } else if (diagonal === maxDiagonal && area > maxArea) {
+            maxArea = area;
+        }
+    }
+
+    return maxArea;
+}
